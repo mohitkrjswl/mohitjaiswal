f71701927ba5c9e860c51bd6aabbef341749c67a
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-05-09 22:44:07 +0530

    basic structure

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-ful">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-10">
+          Thanks for visiting Mohit's Profile 😊 <br></br>
+          I am Mohit Kumar Jaiswal, I am a Computer Science graduate currently pursuing MCA my Post Graduation from Indira Gandhi National Open University(IGNOU) 2023-2025. I have a Bachelor of Computer Application degree from Institute of Management Studies,Noida-62. As a dedicated Web Developer with internship experience at Spotrental. I am currently advancing my expertise in MERN stack development. Leveraging a solid foundation in HTML, CSS, and JavaScript, I have successfully executed projects that demonstrate both technical proficiency and creative problem-solving.
+          Driven by a passion for continuous learning, my technical skill set includes proficiency inHTML, CSS, React.js, Next.js, JavaScript, Node.js, and Tailwind CSS. Additionally, I bring a solid understanding of databases such as MongoDB, MySQL, and hands-on experience with tools like Postman.
+          <h2>Contacts:</h2> <br></br>
+          <p> Email :✉︎ mohitkrjswl@gmail.com <br></br>
+            GitHub: https://github.com/mohitkrjswl <br></br>
+            LinkedIn : https://www.linkedin.com/in/mohitkrjswl/ <br></br>
+            X : https://twitter.com/mohitkrjswl
+          </p>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About;
